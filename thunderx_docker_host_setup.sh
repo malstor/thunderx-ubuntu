0411d14a471b83f119317e6278b67d8a07a4c6dd
Jeremy Turner <jeremy@malstorm.com>
2021-06-14 11:22:49 -0400

    Update thunderx_docker_host_setup.sh

--- thunderx_docker_host_setup.sh
+++ thunderx_docker_host_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# untested, notes from setup for legacy kernel distro w/ thunderx/armv8 docker host
+# notes from setup for legacy kernel distro w/ thunderx/armv8 docker host
 sudo apt-mark hold linux-image-generic linux-headers-generic 
 sudo apt update
 sudo apt install python3.8 python3.8-distutils python3.8-dev libxml2-dev libxslt-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3.8 get-pip.py
-pip3.8 install docker-compose
+sudo pip3.8 install docker-compose
+sudo cp cpt8x-mc-ae.out, cpt8x-mc-se.out /lib/firmware 
